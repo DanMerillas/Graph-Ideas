f49cdcc074313b5113470e96a69baff016b6321d
Daniel Merillas <dmerillasg@minsait.com>
2023-04-23 02:48:12 +0200

    Comienzan principales llamadas a graph

---
+++ src/webparts/todayWork/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ITodayWorkWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'TodayWorkWebPartStrings' {
+  const strings: ITodayWorkWebPartStrings;
+  export = strings;
+}
